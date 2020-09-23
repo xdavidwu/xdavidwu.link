4d4dc46f62e479d6418e11bfccb500ae413f29a3
xdavidwu <xdavidwuph@gmail.com>
2020-09-23 15:53:16 +0800

    remove js sticky class control
    when not as a sidebar, the css (top:) with sticky is invalid for static
    position anyway

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,27 +11,6 @@ $(document).ready(function() {
   // FitVids init
   //$("#main").fitVids();
 
-  // Sticky sidebar
-  var stickySideBar = function() {
-    var show =
-      $(".author__urls-wrapper button").length === 0
-        ? $(window).width() > 1024 // width should match $large Sass variable
-        : !$(".author__urls-wrapper button").is(":visible");
-    if (show) {
-      // fix
-      $(".sidebar").addClass("sticky");
-    } else {
-      // unfix
-      $(".sidebar").removeClass("sticky");
-    }
-  };
-
-  stickySideBar();
-
-  $(window).resize(function() {
-    stickySideBar();
-  });
-
   // Close search screen with Esc key
   $(document).keyup(function(e) {
     if (e.keyCode === 27) {
