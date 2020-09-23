b3c855d4f9294cc480d57c74baa7bc4982ba8e08
xdavidwu <xdavidwuph@gmail.com>
2020-09-23 11:11:49 +0800

    ruby-based anchor

---
+++ _plugins/anchor.rb
@@ -0,0 +1,5 @@
+class Jekyll::MarkdownHeader < Jekyll::Converters::Markdown
+    def convert(content)
+        super.gsub(/<h(\d) id="(.*?)">(.*)<\/h(\d)>/, '<h\1 id="\2">\3<a class="header-link" href="#\2"><span class="sr-only">Permalink</span><i class="fas fa-link"></i></a></h\1>')
+    end
+end
