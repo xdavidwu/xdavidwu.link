f283e884ab3a01608e30193cecdd360f3d75bf01
xdavidwu <xdavidwuph@gmail.com>
2021-05-26 23:41:04 +0800

    js: remove gumshoe

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,7 +2,6 @@
 //require('magnific-popup');
 //require('./plugins/jquery.fitvids');
 require('./_greedy-navigation');
-//const Gumshoe = require('gumshoejs');
 /* ==========================================================================
    jQuery plugin settings and other scripts
    ========================================================================== */
@@ -30,26 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector(".search-content input").focus();
     }, 400);
   });
-
-  // Gumshoe scroll spy init
-  /*if($("nav.toc").length > 0) {
-    var spy = new Gumshoe("nav.toc a", {
-      // Active classes
-      navClass: "active", // applied to the nav list item
-      contentClass: "active", // applied to the content
-
-      // Nested navigation
-      nested: false, // if true, add classes to parents of active link
-      nestedClass: "active", // applied to the parent items
-
-      // Offset & reflow
-      offset: 20, // how far from the top of the page to activate a content area
-      reflow: true, // if true, listen for reflows
-
-      // Event support
-      events: true // if true, emit custom events
-    });
-  }*/
 
   // add lightbox class to all image links
   /*$(
